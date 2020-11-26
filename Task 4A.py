77009168ad455dbd6d9bc6ee5036868a83667c6e
subash2617 <74961842+subash2617@users.noreply.github.com>
2020-11-26 19:47:56 +0530

    Add files via upload
    30 days chellange
    Day 4

---
+++ Task 4A.py
@@ -0,0 +1,13 @@
+ListA= [1,2,3,4,5,6,7]
+ListA.append(8)
+print('8 is added to the list ')
+print(ListA)
+ListA.remove(4)
+print('4 is deleted from the list')
+print(ListA)
+# Storing the largest number from the list to a variable #
+x= max(ListA)
+print(x)
+# Store the Smallest number from the list to a variable #
+y= min(ListA)
+print(y)
---
+++ Task 4B.py
@@ -0,0 +1,9 @@
+# Creating a tuple and printing the reverse of the created tuple #
+TupleA=("26 NOVEMBER 2020")
+x=reversed(TupleA)
+print(tuple(x))
+# Creating a tuple and converting tuple into list #
+TupleB=("apple", "banana", "cherry", "orange")
+ListB=list(TupleB)
+print(ListB)
+print(type(ListB))
